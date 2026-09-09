748773a4d0885eeaf787777a6ff4ca7180e34c35
teeradui <teerada1107@gmail.com>
2026-09-09 21:05:10 +0700

    Fix error catching

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,5 +1,5 @@
 class ApiConfig {
-  static const String baseUrl = 'http://172.20.10.4:3000/api';
+  static const String baseUrl = 'http://10.121.99.253:3000/api';
   static const String trips = '$baseUrl/trips';
   static String tripMembers(String tripId) => '$trips/$tripId/members';
   static String tripInvites(String tripId) => '$trips/$tripId/invites';
